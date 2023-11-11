29dfebed6d5ac01d9636a61b527809802c7a4c21
Kumori <tauen8@gmail.com>
2023-11-10 18:04:05 -0600

    finished basic character layout with all inputs and stats

---
+++ dnd-character/components/GenerateButton.vue
@@ -0,0 +1,10 @@
+<template>
+    <button class="font-semibold bg-white p-2 mx-3 rounded-md  hover:bg-black hover:text-white"> Generate New Race
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'GenerateButton'
+}
+</script>
--- dnd-character/pages/index.vue
+++ dnd-character/pages/index.vue
@@ -1,5 +1,7 @@
 <script setup>
 const { data: randomName } = await useFetch('/api/randomName')
+
+
 </script>
 
 <template >
@@ -11,21 +13,72 @@ const { data: randomName } = await useFetch('/api/randomName')
         <h1 class="font-sans font-bold text-lg grid justify-center "> Choose Your Character</h1>
 
     </div>
-    <div class="grid justify-center">
-        <button class="font-bold bg-white p-2 rounded-md m-4 hover:bg-black hover:text-white"> Generate </button>
-    </div>
-    
-   
-    <div class="m-4 p-3">
-<div class="p-3">
 
-    <p class="p-3"> Name : {{ randomName.firstName }} {{ randomName.lastName }}</p>
-    <p class="p-3"> Race </p>
-    <p class="p-3"> Class </p>
-    <p class="p-3"> Stats </p>
-    
-</div>
+
+
+    <div class="m-4 p-3">
+        <p class="font-bold text-orange-600 my-2"> Choose your own stats or generate randomly!</p>
+        <div class=" font-bold p-3 bg-amber-100 rounded-xl">
+
+            <p class="font-bold p-3"> Name : <input label="chosen-name" placeholder="Chosen Name"
+                    class="mx-2 px-2 rounded-xl"> Random Name
+                : {{
+                    randomName.firstName }} {{ randomName.lastName }}
+                <button class="font-semibold bg-white p-2 mx-3 rounded-md hover:bg-black hover:text-white"> Generate New
+                    Name </button>
+            </p>
+            <p class="p-3"> Race : <input label="chosen-race" placeholder="Chosen Race" class="mx-2 px-2 rounded-xl">
+
+                <button class="font-semibold bg-white p-2 mx-3 rounded-md  hover:bg-black hover:text-white"> Generate New
+                    Race
+                </button>
+            </p>
+            <p class="p-3"> Class : <input label="chosen-class" placeholder="Chosen Class" class="mx-2 px-2 rounded-xl"></p>
+            <div class="bg-white rounded-xl p-3">
+                <p class="p-1"> Stats : </p>
+                <div class="grid grid-cols-5">
+                    <div>
+                        <ul class="mx-6 ">
+                            <li> Attack </li>
+                            <li> Defense </li>
+                            <li> Dexterity </li>
+                            <li> Charisma </li>
+                            <li> Constitution </li>
+                        </ul>
+                    </div>
+                    <div class="grid w-fit ">
+                        <input label="chosen-cons" placeholder="Current Constitution"
+                            class="px-3 mx-2 bg-slate-100 rounded-xl text-sm">
+                        <input label="chosen-cons" placeholder="Current Constitution"
+                            class="px-3 mx-2 bg-slate-100 rounded-xl text-sm">
+                        <input label="chosen-cons" placeholder="Current Constitution"
+                            class="px-3 mx-2 bg-slate-100 rounded-xl text-sm">
+                        <input label="chosen-cons" placeholder="Current Constitution"
+                            class="px-3 mx-2 bg-slate-100 rounded-xl text-sm">
+                        <input label="chosen-cons" placeholder="Current Constitution"
+                            class="px-3 mx-2 bg-slate-100 rounded-xl text-sm">
+                        <input label="chosen-cons" placeholder="Current Constitution"
+                            class="px-3 mx-2 bg-slate-100 rounded-xl text-sm">
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
 
 
     </div>
 </template>
+
+get dropdown of races, and classes
+reformat NavBar
+textfields for info
+<!-- make button component -->
+generate random races
+generate random classes
+generate random stats
+change logo
+figure out colors
+nav logos
+connect button to state change
+pass props to button component
